3a62bcfe24cb39a21141b16cd6610b16180392c3
14rty <mail.14rty@gmail.com>
2022-06-21 15:39:13 +0300

    done 1-5 lab

--- webLab1/ve/Scripts/activate.bat
+++ webLab1/ve/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Asus\Desktop\University\WEB2\WebDevMPU\webLab2\ve
+set VIRTUAL_ENV=C:\Users\Asus\Desktop\University\WEB2\WebDevMPU\webLab1\ve
 
 if not defined PROMPT set PROMPT=$P$G
 
